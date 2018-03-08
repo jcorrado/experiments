0bc92b520322abbc74eb56fbfe620f5b6012bf03
Jereme Corrado <jereme@zoion.net>
2018-03-08 11:44:33 -0500

    Cleaned up seating probability to support testing

--- src/experiments/table-seating-probability.clj
+++ src/experiments/table-seating-probability.clj
@@ -6,29 +6,34 @@
   (:require [clojure.spec.alpha :as s]
             [clojure.math.combinatorics :as combo]))
 
-(defn calc-side-perms
+(defn ^:private calc-side-permutations
   [coll]
   {:pre [(s/valid? even? (count coll))]}
   (map (partial take (/ (count coll) 2))
        (combo/permutations coll)))
 
-(defn report-probabilities
-  [perms]
-  (let [tot (count perms)]
-    (doseq [n (range (-> (first perms) count inc))]
-      (let [cnt (count (filter #(= n (get (frequencies %) \w)) perms))]
-        (println (format "%.2f probability of %d women"
-                         (float (/ cnt tot))
-                         n))))))
+(defn calc-probabilities
+  [coll]
+  (let [perms (calc-side-permutations coll)
+        total (count perms)]
+    (reduce (fn [accum n]
+              (let [cnt (count (filter #(= n (get (frequencies %) \w)) perms))]
+                (assoc accum n (float (/ cnt total)))))
+            {}
+            (range (-> (first perms) count inc)))))
 
-(-> (calc-side-perms (concat (repeat 4 \w)
-                             (repeat 4 \m)
-                             (repeat 2 nil)))
+(defn report-probabilities
+  [m]
+  (doseq [n (keys m)]
+    (println (format "%.3f probability of %d women on one side" (get m n) n))))
+
+(-> (calc-probabilities (concat (repeat 3 \w)
+                                (repeat 4 \m)
+                                (repeat 1 nil)))
     report-probabilities)
 
-;; 0.00 probability of 0 women
-;; 0.24 probability of 1 women
-;; 0.48 probability of 2 women
-;; 0.24 probability of 3 women
-;; 0.02 probability of 4 women
-;; 0.00 probability of 5 women
+;; 0.000 probability of 0 women on one side
+;; 0.429 probability of 1 women on one side
+;; 0.429 probability of 2 women on one side
+;; 0.071 probability of 3 women on one side
+;; 0.000 probability of 4 women on one side
