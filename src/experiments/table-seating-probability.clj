36f8ea07ac5cfe842b4a8253b579dce79869c788
Jereme Corrado <jereme@zoion.net>
2018-03-29 14:08:13 -0400

    Reworked code to not assume two genders.
    The problem is more about clustering by identity and doesn't have any
    requirement for N genders.  The previous "empty" was already
    implemented that way, so not much to change.

--- src/experiments/table-seating-probability.clj
+++ src/experiments/table-seating-probability.clj
@@ -6,18 +6,20 @@
   (:require [clojure.spec.alpha :as s]
             [clojure.math.combinatorics :as combo]))
 
-(defn ^:private calc-side-permutations
+(defn calc-side-permutations
   [coll]
   {:pre [(s/valid? even? (count coll))]}
   (map (partial take (/ (count coll) 2))
        (combo/permutations coll)))
 
 (defn calc-probabilities
-  [coll]
+  [id coll]
   (let [perms (calc-side-permutations coll)
         total (count perms)]
     (reduce (fn [accum n]
-              (let [cnt (count (filter #(= n (get (frequencies %) \w)) perms))]
+              (let [cnt (count (filter (fn [p]
+                                         (= (get (frequencies p) id) n))
+                                       perms))]
                 (assoc accum n (float (/ cnt total)))))
             {}
             (range (-> (first perms) count inc)))))
@@ -25,15 +27,16 @@
 (defn report-probabilities
   [m]
   (doseq [n (keys m)]
-    (println (format "%.3f probability of %d women on one side" (get m n) n))))
+    (println (format "%.3f probability of %d on one side" (get m n) n))))
 
-(-> (calc-probabilities (concat (repeat 3 \w)
-                                (repeat 4 \m)
-                                (repeat 1 nil)))
+(-> (calc-probabilities \w (concat (repeat 3 \w)
+                                   (repeat 3 \m)
+                                   (repeat 1 \o)
+                                   (repeat 1 nil)))
     report-probabilities)
 
-;; 0.000 probability of 0 women on one side
-;; 0.429 probability of 1 women on one side
-;; 0.429 probability of 2 women on one side
-;; 0.071 probability of 3 women on one side
-;; 0.000 probability of 4 women on one side
+;; 0.000 probability of 0 on one side
+;; 0.429 probability of 1 on one side
+;; 0.429 probability of 2 on one side
+;; 0.071 probability of 3 on one side
+;; 0.000 probability of 4 on one side
